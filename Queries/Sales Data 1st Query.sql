0bded55550d21e67b577fc356e66cd3a04d2a648
Aamir Ahmed <77324125+Aamir-Ahmed10@users.noreply.github.com>
2026-08-18 22:17:37 -0700

    Update Sales Data 1st Query.sql

--- Queries/Sales Data 1st Query.sql
+++ Queries/Sales Data 1st Query.sql
@@ -1,1 +1,8 @@
+SELECT * FROM orders
 
+--Find the Total number of Orders by Categories
+SELECT COUNT(Order_number), p.category
+FROM orders AS o
+LEFT JOIN products AS p
+ON o.product_id = p.product_id
+GROUP BY p.Category
